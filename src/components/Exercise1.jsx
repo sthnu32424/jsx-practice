2ee0a960619a216180fe52d55f2f4be1c974a257
horishota6566 <shou6130@gmail.com>
2026-05-20 09:38:29 +0900

    波括弧内での文字列結合を波括弧の組み合わせにする

--- src/components/Exercise1.jsx
+++ src/components/Exercise1.jsx
@@ -4,7 +4,9 @@ const Exercise1 = () => {
   return (
     <ul>
       {users.map((user) => (
-        <li key={user.id}>{user.name + (user.age >= 18 ? "（成人）" : "")}</li>
+        <li key={user.id}>
+          {user.name} {user.age >= 18 ? "（成人）" : ""}
+        </li>
       ))}
     </ul>
   );
