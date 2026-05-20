010237a871c3302de3f35759cb76f96921e7b646
horishota6566 <shou6130@gmail.com>
2026-05-20 09:45:14 +0900

    波括弧内での文字列結合を波括弧の組み合わせにする

--- src/components/Exercise2.jsx
+++ src/components/Exercise2.jsx
@@ -3,13 +3,17 @@ import { devices } from "../data/data";
 const Exercise2 = () => {
   return (
     <ul>
-      {devices.map(device => {
+      {devices.map((device) => {
         if (device.width >= 700) {
-          return <li key={device.id}>{device.name + (device.price >= 1000 ? "💰 高級品" : "🛒 お買い得")}</li>
+          return (
+            <li key={device.id}>
+              {device.name} {device.price >= 1000 ? "💰 高級品" : "🛒 お買い得"}
+            </li>
+          );
         }
       })}
     </ul>
-  )
+  );
 };
 
 export default Exercise2;
